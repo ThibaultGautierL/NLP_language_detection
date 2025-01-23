0e3de39c50da6af69735d464aee562205030f3a3
Faritaz <thibaultgautierl@gmail.com>
2025-01-23 16:49:05 +0100

    data_cleaner_function

--- detection.py
+++ detection.py
@@ -2,8 +2,20 @@ import string
 import pandas as pd
 import numpy as np
 import re
-import marplotlib.pyplot as plt
-import seaborn as sns
+# import marplotlib.pyplot as plt
+# import seaborn as sns
 
 
-pd.read_csv('')
+#Importer le dataset
+df = pd.read_csv('data_set.csv')
+print(df.head(0))
+
+#Traitement de la donnée retirer la ponctuation et les majs
+def remove_ponctuation(text):
+    for pon in string.punctuation:
+        text = text.replace(pon, "")
+    text = text.lower()
+    return(text)
+
+
+print(remove_ponctuation(" Jas _ ' ( % deu: dheuu7 dudeUDEuoo +de"))
