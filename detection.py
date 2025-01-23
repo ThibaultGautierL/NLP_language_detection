f6625ff676f3b04a78bea28bd853dd9f13c89e37
Faritaz <thibaultgautierl@gmail.com>
2025-01-23 17:57:42 +0100

    adding trained data and check accuracy score

--- detection.py
+++ detection.py
@@ -2,13 +2,18 @@ import string
 import pandas as pd
 import numpy as np
 import re
-# import marplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import feature_extraction
+from sklearn import pipeline
+from sklearn import linear_model
+from sklearn import metrics
 
 
 #Importer le dataset
 df = pd.read_csv('data_set.csv')
-print(df.head(5))
+# print(df.head(5))
 
 #Traitement de la donnée retirer la ponctuation et les majs
 def remove_ponctuation(text):
@@ -19,4 +24,19 @@ def remove_ponctuation(text):
 
 df['Text'] = df['Text'].apply(remove_ponctuation)
 
-print(df.head(5))
+# print(df.head(5))
+
+X = df.iloc[:,0]
+Y = df.iloc[:,1]
+
+#0,3% pour les resultats et ,7% pour l'entrainement
+X_traindata, X_testdata, Y_traindata, Y_testdata = train_test_split(X, Y, test_size = 0.4)
+
+#Prend du unigramme au trigramme
+vector= feature_extraction.text.TfidfVectorizer(ngram_range=(1,3), analyzer= 'char')
+
+model_pipe = pipeline.Pipeline([('vector', vector), ('clf', linear_model.LogisticRegression())])
+model_pipe.fit(X_traindata, Y_traindata)
+
+predicted_value = model_pipe.predict(X_testdata)
+print(f"{metrics.accuracy_score(Y_testdata, predicted_value) * 100:.2f} %")
